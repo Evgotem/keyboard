a7b5c5d91e2ff055a73a555aa93a27f872bdd509
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-12-25 14:57:23 +0400

    done

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let input = document.querySelector(".input");
+let items = document.querySelectorAll(".keyboard__item");
+
+function push (event) {
+  items.forEach((elem) => {
+    if (elem.getAttribute("data") === event.key) {
+      elem.classList.add("active");
+    } else {
+      elem.classList.remove("active");
+    }
+  });
+}
+input.onkeydown = push;
+
+
+for (let i = 0; i < items.length; i++) {
+   items[i].onmousedown = () => {
+      items[i].classList.add('active');
+      if (items[i].getAttribute("data") != 'Tab' &&
+          items[i].getAttribute("data") != 'Enter' &&
+          items[i].getAttribute("data") != 'Shift' &&
+          items[i].getAttribute("data") != 'Control' &&
+          items[i].getAttribute("data") != 'Alt'
+      ) {
+         input.value += items[i].getAttribute("data");
+      }
+   }
+   items[i].onmouseup = () => {
+      items[i].classList.remove('active');
+   }
+}
+// items.forEach((elem) => {
+//    elem.classList.remove('active');
+//    elem.onclick = () => {
+//       elem.classList.add('active');
+//    }
+// });
